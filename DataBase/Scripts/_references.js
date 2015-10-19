93cc70aa0ed7a7455459c14e8d08bfbdd5100fb9
pcstx <pcstx@163.com>
2015-10-19 13:36:10 +0800

    数据库下拉框带有搜索功能

--- DataBase/Scripts/_references.js
+++ DataBase/Scripts/_references.js
@@ -48,3 +48,4 @@
 /// <reference path="lib/ligerui/js/plugins/ligerWindow.js" />
 /// <reference path="modernizr-2.6.2.js" />
 /// <reference path="respond.js" />
+/// <reference path="select2.full.min.js" />
